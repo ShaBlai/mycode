c5b08b13ca3823c87e42c9183f18569899a7c1f8
ShaBlai <shawn.blair@tlgcohort.com>
2023-01-09 21:36:31 +0000

    learning about input function

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - concatenate vs print a series of items"""
+
+def main():
+
+    # collect string input from the user
+    user_input = input("Please enter an IPv4 IP address:")
+    
+    ## the line below creates a single string that is passed to print()
+    # print("You told me the IPv4 address is: " + user_input)
+    
+    ## print() can be given a series of objects separated by a comma
+    print("You told me the IPv4 address is:", user_input)
+
+    ##get the vendor information
+    vendor_input = input("Please enter the vendor's name here:")
+
+    ## print the vendor's information
+    print("This was the vendor's name that you submitted", vendor_input)
+
+
+main()
+
