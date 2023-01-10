422c1fc24cc730930b1a6f86091c157d6fb5d8f7
ShaBlai <shawn.blair@tlgcohort.com>
2023-01-10 18:09:03 +0000

    added challenge 31

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import random
 
 my_list =["192.168.0.5", 5060, "UP"]
 
@@ -18,3 +18,26 @@ print("The last item in the list (state): " + my_list[2] )
 iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
 
 print("The ip addresses are " + iplist[3] +" "+ iplist[4])
+
+
+wordbank = ["indentation", "spaces"]
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
+wordbank.append(4)
+
+num = int(input("Please enter a number between 0 and 20"))
+
+student_name =   random.choice(tlgstudents)
+
+
+print(student_name + " always uses " + str(wordbank[2]) + " spaces to indent ") 
+
+
+
+
+
+
+
+
+
+
