f677adcde439c28dc825ae8a82799f0038a6ce67
ShaBlai <shawn.blair@tlgcohort.com>
2023-01-11 17:25:21 +0000

    working on if, elif, else

---
+++ iftest2/ipask.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+
+ipchk = input("Apply for an IP Address!") #prompt user for input
+
+
+if ipchk:
+    print("Looks like the address was set as: " + ipchk) 
+else:
+    print("You did not provide input")#if nothing was provided
+
+
+
+
