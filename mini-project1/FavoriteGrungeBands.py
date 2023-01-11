998a6f6fe72e749dd524f2ff1dfc9b6ef854a9ac
ShaBlai <shawn.blair@tlgcohort.com>
2023-01-11 23:34:52 +0000

    update end of day 3

--- mini-project1/FavoriteGrungeBands.py
+++ mini-project1/FavoriteGrungeBands.py
@@ -3,6 +3,7 @@
 #create a quiz to where a user tries to guess my favorite 90s rock band and play the music associated
 
 import webbrowser
+import random
 
 
 #create guesses and answer
@@ -10,9 +11,9 @@ rockGuesses = 0
 answer = " "
 
 
-alcUrl = "https://www.youtube.com/watch?v=ODTv9Lt5WYs"
-metUrl = "https://www.youtube.com/watch?v=E0ozmU9cJDg"
-pjUrl = "https://www.youtube.com/watch?v=RgStt2i_pSk"
+alcUrl = ["https://www.youtube.com/watch?v=ODTv9Lt5WYs", "https://www.youtube.com/watch?v=9EKi2E9dVY8", "https://www.youtube.com/watch?v=E17hqs4Un-k"]
+metUrl = ["https://www.youtube.com/watch?v=E0ozmU9cJDg", "https://www.youtube.com/watch?v=0JJPqA-KwOw", "https://www.youtube.com/watch?v=CD-E-LDc384"]
+pjUrl = ["https://www.youtube.com/watch?v=RgStt2i_pSk", "https://www.youtube.com/watch?v=JNZezhUkOSk", "https://www.youtube.com/watch?v=CxKWTzr-k6s"]
 
 
 #establish the while loop
@@ -29,20 +30,25 @@ while rockGuesses < 3 and answer !="alice in chains" and answer !="metallica" an
     #lowercase user input
     answerMusic = answerMusic.lower() 
 
-    #if the answer is alice in chains, execute and open the music URL
+    #if the answer is alice in chains, execute and open the random music URL
     if (answerMusic =="alice in chains"):
         print("Congrats, you know your stuff! Time to jam out, I STAYYYY AWAYYYYY")
-        webbrowser.open_new_tab(alcUrl)
+        #uses the web browser to open the URL after a random one from the list has been selected
+        webbrowser.open_new_tab(random.choice(alcUrl))
         break
-   #if the answer is metallica, execute and open the music URL
+    
+   #if the answer is metallica, execute and open the random music URL
     if (answerMusic == "metallica"):
         print("Correct, time to ROCK OUT! My other favorites are Alice In chains and Pearl Jam")
-        webbrowser.open_new_tab(metUrl)
+        #uses the web browser to open the URL after a random one from the list has been selected
+        webbrowser.open_new_tab(random.choice(metUrl))
         break
-    #if the answer is pearl jam, execute and open the music URL    
+    
+    #if the answer is pearl jam, execute and open the random music URL    
     if (answerMusic == "pearl jam"):
         print("Correct, time to listen to Eddie Vedder! My other favorites are Metallica and Alice In Chains")
-        webbrowser.open_new_tab(pjUrl)
+        #uses the web browser to open the URL after a random one from the list has been selected
+        webbrowser.open_new_tab(random.choice(pjUrl))
         break
     
     #output if 2 guesses have been made
