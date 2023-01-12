78596a6eb4b0af039c43cb2d97cc122d5b54e507
ShaBlai <shawn.blair@tlgcohort.com>
2023-01-12 17:47:12 +0000

    Updated the music video URL's to dictionaries

--- mini-project1/FavoriteGrungeBands.py
+++ mini-project1/FavoriteGrungeBands.py
@@ -6,18 +6,18 @@ import webbrowser
 import random
 
 
+
+
 #create guesses and answer
 rockGuesses = 0
 answer = " "
 
-
-alcUrl = ["https://www.youtube.com/watch?v=ODTv9Lt5WYs", "https://www.youtube.com/watch?v=9EKi2E9dVY8", "https://www.youtube.com/watch?v=E17hqs4Un-k"]
-metUrl = ["https://www.youtube.com/watch?v=E0ozmU9cJDg", "https://www.youtube.com/watch?v=0JJPqA-KwOw", "https://www.youtube.com/watch?v=CD-E-LDc384"]
-pjUrl = ["https://www.youtube.com/watch?v=RgStt2i_pSk", "https://www.youtube.com/watch?v=JNZezhUkOSk", "https://www.youtube.com/watch?v=CxKWTzr-k6s"]
-
+#Switched to a dictionary, named the videos associated
+alcVideoURL = {"I Stay Away": "https://www.youtube.com/watch?v=ODTv9Lt5WYs", "Nutshell": "https://www.youtube.com/watch?v=9EKi2E9dVY8", "Down in a Hole": "https://www.youtube.com/watch?v=E17hqs4Un-k"}
+metVideoURL = {"Master of Puppets": "https://www.youtube.com/watch?v=E0ozmU9cJDg", "Fuel": "https://www.youtube.com/watch?v=0JJPqA-KwOw", "Enter Sandman": "https://www.youtube.com/watch?v=CD-E-LDc384"}
+pjVideoURL = {"Black": "https://www.youtube.com/watch?v=RgStt2i_pSk", "Even Flow": "https://www.youtube.com/watch?v=CxKWTzr-k6s","Jeremy": "https://www.youtube.com/watch?v=JNZezhUkOSk"}
 
 #establish the while loop
-
 #if rockguesses are less than three keep going
 while rockGuesses < 3 and answer !="alice in chains" and answer !="metallica" and answer!="pearl jam":
     
@@ -34,25 +34,25 @@ while rockGuesses < 3 and answer !="alice in chains" and answer !="metallica" an
     if (answerMusic =="alice in chains"):
         print("Congrats, you know your stuff! Time to jam out, I STAYYYY AWAYYYYY")
         #uses the web browser to open the URL after a random one from the list has been selected
-        webbrowser.open_new_tab(random.choice(alcUrl))
+        webbrowser.open_new_tab(random.choice(list(alcVideoURL.values())))
         break
     
    #if the answer is metallica, execute and open the random music URL
     if (answerMusic == "metallica"):
         print("Correct, time to ROCK OUT! My other favorites are Alice In chains and Pearl Jam")
         #uses the web browser to open the URL after a random one from the list has been selected
-        webbrowser.open_new_tab(random.choice(metUrl))
+        webbrowser.open_new_tab(random.choice(list(metVideoURL.values())))
         break
     
     #if the answer is pearl jam, execute and open the random music URL    
     if (answerMusic == "pearl jam"):
         print("Correct, time to listen to Eddie Vedder! My other favorites are Metallica and Alice In Chains")
         #uses the web browser to open the URL after a random one from the list has been selected
-        webbrowser.open_new_tab(random.choice(pjUrl))
+        webbrowser.open_new_tab(random.choice(list(pjVideoURL.values())))
         break
     
     #output if 2 guesses have been made
-    elif rockGuesses==2:
+    elif rockGuesses == 2:
         print("You have one more guess...so much pressure (Hint: My friend ALICE talks about METAL with PEARL)")    
     #output if user runs out of guesses
     elif rockGuesses == 3:
