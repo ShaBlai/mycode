d026b702e0d44ad57c9a1aa62650b22be1a0521b
ShaBlai <shawn.blair@tlgcohort.com>
2023-01-13 20:06:24 +0000

    added the mulligan cheat class for the dice & learning objects/classes

--- oop2/dice_test.py
+++ oop2/dice_test.py
@@ -8,14 +8,16 @@ Creating a simple dice program utilizing classes."""
 #unimported player, as it is not needed
 from cheatdice import Cheat_Swapper
 from cheatdice import Cheat_Loaded_Dice
+from cheatdice import Cheat_Mulligan
 
 def main():
     """run-time code"""
 
     # create two cheater objects
     cheater1 = Cheat_Swapper() # ability is to change 3rd dice roll to 6
-    cheater2 = Cheat_Loaded_Dice() # increase all rolls by +1 provided they are < 6
-
+    #cheater2 = Cheat_Loaded_Dice() # increase all rolls by +1 provided they are < 6
+    cheater2 = Cheat_Mulligan()
+    
     # both players take turns
     cheater1.roll()
     cheater2.roll()
