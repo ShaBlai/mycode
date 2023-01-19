cbbd9487c505dcd44aec3fe59a60b0cb5b8e7630
ShaBlai <shawn.blair@tlgcohort.com>
2023-01-19 17:39:38 +0000

    Added API redirects and limits

---
+++ flaskapi/51lookups.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+
+
+import os
+
+
+def main():
+    
+    count = 0
+    
+    while (count <= 51):
+        os.system("curl http://0.0.0.0:2224/fast")
+        count+=1
+        print("Fast go brrrrr")
+        
+main()        
+        
+        
+        
+        
+    
+    
+    
